df7ce71c5b052a3f6224fed2bd7b4c24187f118b
Aleksandr Stroitelev <36483253+AlexanderStroitelev@users.noreply.github.com>
2024-05-16 22:58:58 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,36 @@
+# Задача Сложная сдача
+price = 34
+weight = 4.5
+money = 500
+cash_back = money - price * weight
+print (float(cash_back))
+
+
+# Задача Сдача всем
+price = 25.14
+weight = 10.67
+money = 1000
+cash_back = money - price * weight
+print (float(cash_back))
+
+# Задача Работаем с выводом данных
+name = 'Черешня'
+price = 25
+weight = 10
+money = 1000
+sum = price * weight
+cash_back = money - price * weight
+print('Чек'+' '+name+' - '+str(weight)+'кг'+' - '+str(price)+'руб/кг'+' Итого: '
+      +str(sum)+' руб'+' Внесено: '+str(money)+' руб'+' Сдача: '+str(cash_back)+' руб')
+
+
+# задача Самая простая задача на свете
+j = 5
+for i in range(j):
+    print('Купи конструктор!')
+
+# задача Автоматизируй простоту
+a = 5
+deal = 'читать книги'
+for i in range(a):
+    print('Обожаю писать ' + '"' + str(deal) + '"')
