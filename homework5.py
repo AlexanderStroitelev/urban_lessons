71a10b866fd1fc93f14b87e464d45f872de4e4ae
Aleksandr Stroitelev <36483253+AlexanderStroitelev@users.noreply.github.com>
2024-05-19 23:16:29 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+my_list = ['banana','apple','peach','limon','plum','orange']
+print(my_list)
+print('первый - ' + my_list[0], 'и последний - ' + my_list[-1])
+print(my_list[2:4])
+my_list[2] = 'kiwi'
+print(my_list)
+
+my_dict = {'рука': 'hand',
+           'голова': 'head',
+           'нога': 'leg',
+           'год': 'year',
+}
+print(my_dict)
+print(my_dict['рука'])
+my_dict['лето'] = 'summer'
+print(my_dict)
