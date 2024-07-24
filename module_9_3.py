c1da92df220fbf85f746958c240672984f5c593d
Aleksandr Stroitelev <36483253+AlexanderStroitelev@users.noreply.github.com>
2024-07-24 22:20:16 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,8 +1,8 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-first_result = (abs(len(s1) - len(s2)) for s1, s2 in zip(first, second) if len(s1) != len(s2))
-second_result = (s1 == s2 for i in range(len(first)) for s1, s2 in [(first[i], second[i])])
+first_result = (abs(len(x) - len(y)) for x, y in zip(first, second) if len(x) != len(y))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
 
 print(list(first_result))
 print(list(second_result))
