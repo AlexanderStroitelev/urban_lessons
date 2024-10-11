752adf6b0421c5e93ede1494af7c88b913b16078
Aleksandr Stroitelev <36483253+AlexanderStroitelev@users.noreply.github.com>
2024-10-11 23:31:07 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -16,12 +16,10 @@ async def user(
         user_id: Annotated
         [
             int, Path
-                (
-                ge=1,
+                (ge=1,
                 le=100,
                 description="Enter User ID",
-                example="1"
-            )
+                example="1")
         ]
 ):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
@@ -31,22 +29,18 @@ async def user_info(
         username: Annotated
         [
             str, Path
-                (
-                min_length=5,
+                (min_length=5,
                 max_length=20,
                 description="Enter username",
-                example="UrbanUser"
-            )
+                example="UrbanUser")
         ],
         age: Annotated
         [
             int, Path
-                (
-                ge=18,
+                (ge=18,
                 le=120,
                 description="Enter age",
-                example="24"
-            )
+                example="24")
         ]
 ):
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
